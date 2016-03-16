7ee436f53068bcda4820faa9787ed7cf768f355c
kat markava <katryc@outlook.com>
2016-03-16 00:15:23 -0400

    fixed the buttons

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,11 @@ class Appointment < ActiveRecord::Base
 validates :start_time, :end_time, :first_name, :last_name, presence: {message: ":Please, fill in the fields."}
 
 
+def availability
+  Appointment.where("start_time > '10:00:00' and end_time < '12:00:00'")
+
+end
+
 
 
 

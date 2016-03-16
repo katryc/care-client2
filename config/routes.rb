447b3cc281130880fd295698b9870bf050d77997
kat markava <katryc@outlook.com>
2016-03-16 01:34:29 -0400

    fixed the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :appointments
-  root 'appointments#index'
+  resources :landings
+  root 'landing#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
